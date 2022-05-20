674e2f29a71bf1007932a84862d38bd9a1566c69
nsobotka <90986529+nsobotka@users.noreply.github.com>
2022-05-20 13:12:46 -0400

    Fixed swap and added newBoard

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, request, redirect, url_for
+from flask import render_template, request, redirect, url_for
 from app import app
 from waffleLogic import *
 
@@ -7,22 +7,40 @@ scrambledPuzzle = scramble(solvedPuzzle)
 states = getStates(solvedPuzzle, scrambledPuzzle)
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods = ['GET'])
 
 def index():
     return render_template('index.html', puzzle = scrambledPuzzle, colors = states)
 
-@app.route('/swap', methods = ['POST'])
+@app.route('/newBoard', methods = ['POST'])
+def newBoard():
+    global states
+    global scrambledPuzzle
+    global solvedPuzzle
+    solvedPuzzle = getPuzzle()
+    scrambledPuzzle = scramble(solvedPuzzle)
+    states = getStates(solvedPuzzle, scrambledPuzzle)
+    return redirect(url_for('index'))
+
+@app.route('/swap', methods = ['GET', 'POST'])
 def swap():
     global states
     global scrambledPuzzle
     global solvedPuzzle
     box1 = request.form['box1']
     box2 = request.form['box2']
-    coord1 = int(box1[3]) - 1
-    coord2 = int(box2[3]) - 1
+    if len(box1) == 4:
+        coord1 = int(box1[3]) - 1
+    else: 
+        coord1 = int(box1[3]) * 10 + int(box1[4]) - 1
+    if len(box2) == 5:
+        coord2 = int(box2[3]) * 10 + int(box2[4]) - 1
+    else: 
+        coord2 = int(box2[3]) - 1
     tempChar = scrambledPuzzle[math.floor(coord1 / 5)][coord1 % 5]
     scrambledPuzzle[math.floor(coord1 / 5)][coord1 % 5] = scrambledPuzzle[math.floor(coord2 / 5)][coord2 % 5]
     scrambledPuzzle[math.floor(coord2 / 5)][coord2 % 5] = tempChar
     states = getStates(solvedPuzzle, scrambledPuzzle)
+    item = render_template('index.html', puzzle = scrambledPuzzle, colors = states)
+    # print(item)
     return render_template('index.html', puzzle = scrambledPuzzle, colors = states)
