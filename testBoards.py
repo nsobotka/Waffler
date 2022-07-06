98d23a411266e7e39b8c16ea0e768f19ffd1e6bc
nsobotka <90986529+nsobotka@users.noreply.github.com>
2022-07-05 23:06:13 -0400

    Test boards

---
+++ testBoards.py
@@ -0,0 +1,141 @@
+
+#Board 1
+scrambled1 = [['C', 'E', 'Y', 'A', 'S'], 
+             ['P', ' ', 'E', ' ', 'M'],
+             ['L', 'D', 'P', 'H', 'O'],
+             ['R', ' ', 'U', ' ', 'R'],
+             ['S', 'E', 'U', 'A', 'P']]
+
+#6... = GREEN, #E9... = YELLOW, #ED... = GREY
+color1 = [[('#6fb05c', '#FFFFFF'), ('#edeff1', '#000000'), ('#edeff1', '#FFFFFF'), ('#e9ba3a', '#000000'), ('#6fb05c', '#FFFFFF')], 
+         [('#edeff1', '#FFFFFF'), ' ', ('#e9ba3a', '#FFFFFF'), ' ', ('#edeff1', '#FFFFFF')],
+         [('#edeff1', '#000000'), ('#edeff1', '#FFFFFF'), ('#6fb05c', '#FFFFFF'), ('#edeff1', '#FFFFFF'), ('#e9ba3a', '#000000')],
+         [('#e9ba3a', '#FFFFFF'), ' ', ('#edeff1', '#000000'), ' ', ('#e9ba3a', '#000000')],
+         [('#6fb05c', '#FFFFFF'), ('#e9ba3a', '#FFFFFF'), ('#edeff1', '#000000'), ('#edeff1', '#FFFFFF'), ('#6fb05c', '#FFFFFF')]]
+
+correct1 = [['C', 'A', 'M', 'P', 'S'],
+           ['U', ' ', 'A', ' ', 'Y'],
+           ['R', 'O', 'P', 'E', 'R'],
+           ['D', ' ', 'L', ' ', 'U'],
+           ['S', 'H', 'E', 'E', 'P']]
+
+
+#Board 2
+scrambled2 = [['R', 'A', 'U', 'E', 'T'], 
+             ['A', ' ', 'T', ' ', 'E'],
+             ['L', 'T', 'C', 'U', 'I'],
+             ['L', ' ', 'O', ' ', 'L'],
+             ['E', 'U', 'V', 'C', 'E']]
+
+#6... = GREEN, #E9... = YELLOW, #ED... = GREY
+color2 = [[('#6fb05c', '#FFFFFF'), ('#edeff1', '#000000'), ('#e9ba3a', '#FFFFFF'), ('#e9ba3a', '#000000'), ('#6fb05c', '#FFFFFF')], 
+         [('#edeff1', '#FFFFFF'), ' ', ('#e9ba3a', '#FFFFFF'), ' ', ('#edeff1', '#FFFFFF')],
+         [('#e9ba3a', '#000000'), ('#edeff1', '#FFFFFF'), ('#6fb05c', '#FFFFFF'), ('#edeff1', '#FFFFFF'), ('#edeff1', '#000000')],
+         [('#edeff1', '#FFFFFF'), ' ', ('#edeff1', '#000000'), ' ', ('#6fb05c', '#000000')],
+         [('#6fb05c', '#FFFFFF'), ('#edeff1', '#FFFFFF'), ('#edeff1', '#000000'), ('#edeff1', '#FFFFFF'), ('#6fb05c', '#FFFFFF')]]
+
+correct2 = [['R', 'E', 'C', 'U', 'T'],
+           ['E', ' ', 'A', ' ', 'U'],
+           ['V', 'O', 'C', 'A', 'L'],
+           ['U', ' ', 'T', ' ', 'L'],
+           ['E', 'L', 'I', 'T', 'E']]
+
+#Board 3
+scrambled3 = [['T', 'C', 'T', 'E', 'E'], 
+             ['B', ' ', 'H', ' ', 'R'],
+             ['C', 'E', 'T', 'S', 'O'],
+             ['T', ' ', 'A', ' ', 'H'],
+             ['O', 'E', 'O', 'L', 'R']]
+
+#6... = GREEN, #E9... = YELLOW, #ED... = GREY
+color3 = [[('#6fb05c', '#FFFFFF'), ('#edeff1', '#000000'), ('#edeff1', '#FFFFFF'), ('#edeff1', '#000000'), ('#6fb05c', '#FFFFFF')], 
+         [('#edeff1', '#FFFFFF'), ' ', ('#e9ba3a', '#FFFFFF'), ' ', ('#edeff1', '#FFFFFF')],
+         [('#e9ba3a', '#000000'), ('#6fb05c', '#FFFFFF'), ('#6fb05c', '#FFFFFF'), ('#edeff1', '#FFFFFF'), ('#edeff1', '#000000')],
+         [('#edeff1', '#FFFFFF'), ' ', ('#edeff1', '#000000'), ' ', ('#e9ba3a', '#000000')],
+         [('#6fb05c', '#FFFFFF'), ('#e9ba3a', '#FFFFFF'), ('#e9ba3a', '#000000'), ('#edeff1', '#FFFFFF'), ('#6fb05c', '#FFFFFF')]]
+
+correct3 = [['T', 'A', 'B', 'L', 'E'],
+           ['O', ' ', 'O', ' ', 'T'],
+           ['R', 'E', 'T', 'C', 'H'],
+           ['S', ' ', 'C', ' ', 'E'],
+           ['O', 'T', 'H', 'E', 'R']]
+
+#Board 4
+scrambled4 = [['E', 'E', 'A', 'T', 'S'], 
+             ['L', ' ', 'R', ' ', 'T'],
+             ['E', 'H', 'U', 'U', 'G'],
+             ['L', ' ', 'O', ' ', 'A'],
+             ['R', 'N', 'T', 'A', 'Y']]
+
+#6... = GREEN, #E9... = YELLOW, #ED... = GREY
+color4 = [[('#6fb05c', '#FFFFFF'), ('#edeff1', '#000000'), ('#e9ba3a', '#FFFFFF'), ('#e9ba3a', '#000000'), ('#6fb05c', '#FFFFFF')], 
+         [('#edeff1', '#FFFFFF'), ' ', ('#edeff1', '#FFFFFF'), ' ', ('#edeff1', '#FFFFFF')],
+         [('#e9ba3a', '#000000'), ('#edeff1', '#FFFFFF'), ('#6fb05c', '#FFFFFF'), ('#edeff1', '#FFFFFF'), ('#e9ba3a', '#000000')],
+         [('#edeff1', '#FFFFFF'), ' ', ('#edeff1', '#000000'), ' ', ('#edeff1', '#000000')],
+         [('#6fb05c', '#FFFFFF'), ('#edeff1', '#FFFFFF'), ('#6fb05c', '#000000'), ('#e9ba3a', '#FFFFFF'), ('#6fb05c', '#FFFFFF')]]
+
+correct4 = [['E', 'T', 'H', 'O', 'S'],
+           ['A', ' ', 'A', ' ', 'U'],
+           ['G', 'R', 'U', 'E', 'L'],
+           ['E', ' ', 'N', ' ', 'L'],
+           ['R', 'A', 'T', 'T', 'Y']]
+
+#Board 5
+scrambled5 = [['G', 'A', 'C', 'U', 'E'], 
+             ['L', ' ', 'O', ' ', 'R'],
+             ['R', 'N', 'L', 'A', 'W'],
+             ['L', ' ', 'O', ' ', 'A'],
+             ['Y', 'M', 'M', 'A', 'Y']]
+
+#6... = GREEN, #E9... = YELLOW, #ED... = GREY
+color5 = [[('#6fb05c', '#FFFFFF'), ('#e9ba3a', '#000000'), ('#e9ba3a', '#FFFFFF'), ('#edeff1', '#000000'), ('#6fb05c', '#FFFFFF')], 
+         [('#edeff1', '#FFFFFF'), ' ', ('#edeff1', '#FFFFFF'), ' ', ('#e9ba3a', '#FFFFFF')],
+         [('#e9ba3a', '#000000'), ('#edeff1', '#FFFFFF'), ('#6fb05c', '#FFFFFF'), ('#6fb05c', '#FFFFFF'), ('#edeff1', '#000000')],
+         [('#edeff1', '#FFFFFF'), ' ', ('#6fb05c', '#000000'), ' ', ('#e9ba3a', '#000000')],
+         [('#6fb05c', '#FFFFFF'), ('#edeff1', '#FFFFFF'), ('#edeff1', '#000000'), ('#e9ba3a', '#FFFFFF'), ('#6fb05c', '#FFFFFF')]]
+
+correct5 = [['G', 'R', 'A', 'C', 'E'],
+           ['U', ' ', 'L', ' ', 'A'],
+           ['M', 'O', 'L', 'A', 'R'],
+           ['M', ' ', 'O', ' ', 'L'],
+           ['Y', 'A', 'W', 'N', 'Y']]
+
+#Board 6
+scrambled6 = [['P', 'E', 'E', 'T', 'D'], 
+             ['R', ' ', 'U', ' ', 'A'],
+             ['A', 'T', 'T', 'L', 'O'],
+             ['E', ' ', 'P', ' ', 'V'],
+             ['L', 'A', 'A', 'R', 'A']]
+
+#6... = GREEN, #E9... = YELLOW, #ED... = GREY
+color6 = [[('#6fb05c', '#FFFFFF'), ('#edeff1', '#000000'), ('#e9ba3a', '#FFFFFF'), ('#edeff1', '#000000'), ('#6fb05c', '#FFFFFF')], 
+         [('#edeff1', '#FFFFFF'), ' ', ('#edeff1', '#FFFFFF'), ' ', ('#edeff1', '#FFFFFF')],
+         [('#e9ba3a', '#000000'), ('#edeff1', '#FFFFFF'), ('#6fb05c', '#FFFFFF'), ('#e9ba3a', '#FFFFFF'), ('#edeff1', '#000000')],
+         [('#edeff1', '#FFFFFF'), ' ', ('#edeff1', '#000000'), ' ', ('#edeff1', '#000000')],
+         [('#6fb05c', '#FFFFFF'), ('#6fb05c', '#FFFFFF'), ('#edeff1', '#000000'), ('#e9ba3a', '#FFFFFF'), ('#6fb05c', '#FFFFFF')]]
+
+correct6 = [['P', 'R', 'O', 'U', 'D'],
+           ['A', ' ', 'T', ' ', 'E'],
+           ['P', 'E', 'T', 'A', 'L'],
+           ['A', ' ', 'E', ' ', 'T'],
+           ['L', 'A', 'R', 'V', 'A']]
+
+#Board 7
+scrambled7 = [['M', 'H', 'I', 'E', 'L'], 
+             ['E', ' ', 'T', ' ', 'E'],
+             ['N', 'M', 'A', 'A', 'R'],
+             ['C', ' ', 'A', ' ', 'D'],
+             ['H', 'A', 'R', 'K', 'D']]
+
+#6... = GREEN, #E9... = YELLOW, #ED... = GREY
+color7 = [[('#6fb05c', '#FFFFFF'), ('#edeff1', '#000000'), ('#edeff1', '#FFFFFF'), ('#e9ba3a', '#000000'), ('#6fb05c', '#FFFFFF')], 
+         [('#edeff1', '#FFFFFF'), ' ', ('#edeff1', '#FFFFFF'), ' ', ('#e9ba3a', '#FFFFFF')],
+         [('#e9ba3a', '#000000'), ('#edeff1', '#FFFFFF'), ('#6fb05c', '#FFFFFF'), ('#edeff1', '#FFFFFF'), ('#edeff1', '#000000')],
+         [('#6fb05c', '#FFFFFF'), ' ', ('#e9ba3a', '#000000'), ' ', ('#edeff1', '#000000')],
+         [('#6fb05c', '#FFFFFF'), ('#e9ba3a', '#FFFFFF'), ('#e9ba3a', '#000000'), ('#edeff1', '#FFFFFF'), ('#6fb05c', '#FFFFFF')]]
+
+correct7 = [['M', 'E', 'D', 'A', 'L'],
+           ['A', ' ', 'R', ' ', 'I'],
+           ['T', 'H', 'A', 'N', 'K'],
+           ['C', ' ', 'M', ' ', 'E'],
+           ['H', 'E', 'A', 'R', 'D']]
